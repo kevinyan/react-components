e93bb4693aa343318a16dc49986f00e35e569658
KAI <kevinyan@users.noreply.github.com>
2019-11-05 18:22:03 +0800

    fix

--- @super-fe/components2/index.js
+++ @super-fe/components2/index.js
@@ -8,7 +8,7 @@ const Item02 = () => (
     <header>
         <h1>Preact Item02 Components</h1>
         <Imgleft />
-        <h2>Item02 组件里的文案</h2>
+        <h2>Item02 组件里的文案002</h2>
         <Textright />
     </header>
 );
